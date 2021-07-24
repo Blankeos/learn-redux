bb763b994b1c8c2d1120a997013733aebdc9ec40
Blankeos <carloantonioct@gmail.com>
2021-07-24 21:34:21 +0800

    test attempt for usestate and redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, signIn } from "./actions";
+import { useState } from "react";
 
 function App() {
   // Get the values from the state using useSelector
   const counter = useSelector((state) => state.counter);
   const isLogged = useSelector((state) => state.isLogged);
+  const [incrementValue, setIncrementValue] = useState(1);
 
   // Changing the state by dispatching actions with useDispatch
   const dispatch = useDispatch();
@@ -12,17 +14,20 @@ function App() {
   return (
     <div className="App">
       <header>
+        <h2>Increment By: {incrementValue}</h2>
+        <button onClick={setIncrementValue((prev) => prev + 1)}>^</button>
+        <button onClick={setIncrementValue((prev) => prev - 1)}>v</button>
         <h1>Counter: {counter}</h1>
         <button
           onClick={() => {
-            dispatch(decrement(5));
+            dispatch(decrement(incrementValue));
           }}
         >
           - Decrement
         </button>
         <button
           onClick={() => {
-            dispatch(increment());
+            dispatch(increment(incrementValue));
           }}
         >
           Increment +
